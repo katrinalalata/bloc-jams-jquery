9b9d091f9ec77c3bdb39acc39ad4c595604c8766
Katrina <katrina.lalata@gmail.com>
2019-05-14 18:31:06 -0400

    resubmit blocjams sub

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -2,5 +2,5 @@
      $('#album-title').text(album.title);
      $('img#album-cover-art').attr('src', album.albumArtUrl);
      $('#release-info').text(album.releaseInfo);
-     $('#artist').text(album.artist);
+     $('.artist').text(album.artist);
 }
