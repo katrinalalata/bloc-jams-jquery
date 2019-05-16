1bcf46ad129135dc8246b88903d097dc714d83bc
Katrina <katrina.lalata@gmail.com>
2019-05-16 13:05:57 -0400

    second commit

--- scripts/helper.js
+++ scripts/helper.js
@@ -3,7 +3,7 @@ class Helper {
   player.playPause (song);
   $('#time-control .current-time').text( currentTime );
   $('#time-control input').val(percent);
-}, 500);
+}, 1000);
 }
 
 var helper = new Helper ();
