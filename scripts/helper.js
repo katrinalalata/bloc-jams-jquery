7ea32cc64292891441b79370a7dfa47de076ef44
Katrina <katrina.lalata@gmail.com>
2019-05-18 09:41:17 -0400

    helper.js update III

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,8 +1,16 @@
 class Helper {
+
+
   playPauseAndUpdate(song) {
-    helper.playPauseAndUpdate (song);
+    player.playPause (song);
+    const totalTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (totalTime / duration) * 100;
     $('#time-control .total-time').text( totalTime );
+    $('#time-control input').val(percent);
+  }
+
 
   }
-}
+
 var helper = new Helper ();
